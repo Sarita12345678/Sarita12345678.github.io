bc6089f96648955c0a62ebfd88f77f46dd43bc4e
Sara Alanis <155699182+Sarita12345678@users.noreply.github.com>
2024-01-25 19:01:23 -0600

    Add files via upload

--- javas.js
+++ javas.js
@@ -1,19 +1,32 @@
+
 function validateForm() {
+    var emailInput = document.getElementById('email');
+    var password = document.getElementById('password');
+    var emailvalidacion = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
 
-        
-    var email = document.getElementById('email');
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailvalidacion.test(emailInput.value)) {
+      window.alert('Ingresa un correo electrónico válido.');    
+      console.log('Ingresa un correo electrónico válido.');
+      
+      
+      return false;
 
-    if (!emailRegex.test(email)) {
-        console.log('Por favor, introduce un correo electrónico válido.');
+      
+
+
+    } 
+    
+    if (password.value.length < 8) {
+        window.alert('La contraseña debe tener al menos 8 caracteres.');
+        console.log('La contraseña debe tener al menos 8 caracteres.');
         return false;
-    }
-    else{
-        window.alert('Sesion iniciada')
-        return false;
-    }
-
+      } 
    
 
-    return true;
-}
+
+    else {
+      window.alert('Sesion iniciada')
+      return true;
+    }  }
+
+  
